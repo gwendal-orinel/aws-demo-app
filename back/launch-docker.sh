91cd306fc275a99e58e98170d75a04b2396633d7
root <root@tscnpc98.vrf.services.thales>
2019-01-29 15:19:09 +0100

    simplification de l'app

--- back/launch-docker.sh
+++
@@ -1,3 +0,0 @@
-docker build -t node-js .
-docker rm -f node-js
-docker run -dit -p 8080:8080 -v $(pwd):/demo/back --name=node-js node-js bash
--- front/launch-docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --name=apache -v $(pwd):/usr/local/apache2/htdocs/ -dit -p 80:80 httpd
